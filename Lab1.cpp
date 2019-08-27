99ad5b579b5a88cb6feb6986f30ee2f1202ffbb3
Andrew Jowe <ajowe034@insite.4cd.edu>
2019-08-27 16:31:13 -0700

    Convert temp from F to C and change temp to float

--- Lab1.cpp
+++ Lab1.cpp
@@ -11,6 +11,7 @@ using std::endl;
 #include <string>
 using std::string;
 
+float convertTempFToC (float temp_f);
 void printId(string assignment);
 
 int main()
@@ -19,7 +20,8 @@ int main()
 
 	short int age; //Store the user's age
 	string name; //Store the user's name
-	short int temp; //Store the user's temp
+	float temp_f; //Store the user's temp in F
+	float temp_c; //Store the user's temp in C
 	string buf; //Use as buffer for getting inputs
 
 	//Get age input
@@ -32,14 +34,17 @@ int main()
 	getline(cin, name);
 
 	//Get temp input
-	cout << "Please input temperature: ";
+	cout << "Please input temperature (degrees F): ";
 	getline(cin, buf);
-	temp = atof(buf.c_str());
+	temp_f = atof(buf.c_str());
+
+	//Convert the input temp from F to C
+	temp_c = convertTempFToC(temp_f);
 
 	//Display Results
 	cout << name << " is " << age << " years old now, and will be " << age + 2 
-		<< " two years from now. It's " << temp 
-		<< " degrees F in San Ramon." << endl;
+		<< " two years from now. It's " << temp_f 
+		<< " degrees F in San Ramon -- that's " << temp_c << " degrees C." << endl;
 
 	return 0;
 }
@@ -59,4 +64,15 @@ void printId(string assignment) {
 	cout << "Compiler Used: Visual Studio Compiler" << endl;
 	cout << "File: " << __FILE__ << endl;
 	cout << "Compiled: " << __DATE__ << endl;
+}
+
+/**********************************************************************
+ * Purpose: Convert the temperature from F to C using math
+ * 
+ * Parameters: float temp_f - The given temperature in F
+ * 
+ * Return: Float temperature in C
+ *********************************************************************/
+float convertTempFToC (float temp_f) {
+	return (temp_f - 32) * 5 / 9;
 }
