9393ee3f705e42278d4563bbf330423a764273c2
Andrew Jowe <ajowe034@insite.4cd.edu>
2019-08-29 23:12:22 -0700

    Lab 2 - Print ID information into console

--- Lab2.cpp
+++ Lab2.cpp
@@ -1,1 +1,39 @@
+//Andrew
+//Lab 2, Making Exact Change
+//Editor Used: Visual Studio Code
+//Compiler Used: Visual Studio Developer Command Prompt
 
+#include <iostream>
+using std::ios;
+using std::cout;
+using std::cin;
+using std::endl;
+
+#include <string>
+using std::string;
+
+void printId(string assignment);
+
+int main() 
+{
+    printId("Lab 2"); //Print information about ID
+
+    return 0;
+}
+
+/**********************************************************************
+* Purpose: Print who typed up this program and the assignment name in the console
+*	Also print name of file and date into the console
+*
+* Parameters: string assignment - assignment name
+*
+* Return: Nothing
+**********************************************************************/
+void printId(string assignment) {
+	cout << "Andrew Jowe" << endl;
+	cout << assignment << endl;
+	cout << "Editor Used: Visual Studio Code" << endl;
+	cout << "Compiler Used: Visual Studio Developer Command Prompt" << endl;
+	cout << "File: " << __FILE__ << endl;
+	cout << "Compiled: " << __DATE__ << endl;
+}
