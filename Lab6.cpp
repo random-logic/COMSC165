b1da389fe4f4976c95760e3beca960230cdb2377
Andrew Jowe <andrewjowe@yahoo.com>
2019-09-09 21:05:52 -0700

    Create Lab 6 file

---
+++ Lab6.cpp
@@ -0,0 +1,36 @@
+//Andrew
+//Lab 6, Course Objects With Serializations
+//Editor Used: Visual Studio Code
+//Compiler Used: Visual Studio Developer Command Prompt
+
+#include <iostream>
+using std::ios;
+using std::cout;
+using std::cin;
+using std::endl;
+
+#include <string>
+using std::string;
+
+void printId(string assignment);
+
+int main() {
+    printID("Lab 6");
+}
+
+/**********************************************************************
+* Purpose: Print who typed up this program and the assignment name in the console
+*	Also print name of file and date into the console
+*
+* Parameters: string assignment - assignment name
+*
+* Return: Nothing
+**********************************************************************/
+void printId(string assignment) {
+	cout << "Andrew Jowe" << endl;
+	cout << assignment << endl;
+	cout << "Editor Used: Visual Studio Code" << endl;
+	cout << "Compiler Used: Visual Studio Developer Command Prompt" << endl;
+	cout << "File: " << __FILE__ << endl;
+	cout << "Compiled: " << __DATE__ << endl;
+}
