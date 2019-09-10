e5d7591f2b22c0a726fb7fa29d31e45fcf44ec36
Andrew Jowe <andrewjowe@yahoo.com>
2019-09-09 21:01:08 -0700

    Fix some minor bugs, add 50 cent coin

--- Lab2.cpp
+++ Lab2.cpp
@@ -18,24 +18,25 @@ int main()
 {
     printId("Lab 2"); //Print information about ID
 
-	double amount_tendered; //Store the amount of money tendered
-	double purchase_amount; //Store the amount of money for the purchase
+	double amount_tendered; //Store the change_due of money tendered
+	double purchase_amount; //Store the change_due of money for the purchase
 	string buf; //Use as buffer for getting inputs
 	double change_due; //Store the change due from purchase value calculated
 
-	int bills_100 = 0; //Stores the amount of each bill
+    int bills_100 = 0; //Stores the change_due of each bill
 	int bills_50 = 0;
 	int bills_20 = 0;
 	int bills_10 = 0;
 	int bills_5 = 0;
 	int bills_1 = 0;
+    int coins_50 = 0;
 	int coins_25 = 0;
 	int coins_10 = 0;
 	int coins_5 = 0;
 	int coins_1 = 0;
 
 	//Get user input
-	cout << "Input the purchase amount and amount tendered with a space seperating the two values: ";
+	cout << "Input the purchase change_due and change_due tendered with a space seperating the two values: ";
 	cin >> buf;
 	purchase_amount = atof(buf.c_str());
 	cin >> buf;
@@ -51,53 +52,58 @@ int main()
 	cout.unsetf (ios::floatfield);
 	cout.unsetf(ios::fixed);
 
-	//Give the exact change
-	while (change_due > 99.9999) {
+    //Give the exact change
+	while (change_due > 99.999999999) {
 		change_due -= 100;
 		bills_100 += 1;
 	}
 
-	while (change_due > 49.9999) {
+	while (change_due > 49.999999999) {
 		change_due -= 50;
 		bills_50 += 1;
 	}
 
-	while (change_due > 19.9999) {
+	while (change_due > 19.999999999) {
 		change_due -= 20;
 		bills_20 += 1;
 	}
 
-	while (change_due > 9.9999) {
+	while (change_due > 9.999999999) {
 		change_due -= 10;
 		bills_10 += 1;
 	}
 
-	while (change_due > 4.9999) {
+	while (change_due > 4.999999999) {
 		change_due -= 5;
 		bills_5 += 1;
 	}
 
-	while (change_due > 0.9999) {
+	while (change_due > 0.999999999) {
 		change_due -= 1;
 		bills_1 += 1;
 	}
 
-	while (change_due > 0.2499) {
+    while (change_due > 0.499999999) {
+		change_due -= 0.50;
+		coins_50 += 1;
+	}
+
+	while (change_due > 0.249999999) {
 		change_due -= 0.25;
 		coins_25 += 1;
 	}
 
-	while (change_due > 0.0999) {
+	while (change_due > 0.099999999) {
 		change_due -= 0.10;
 		coins_10 += 1;
 	}
 
-	while (change_due > 0.0499) {
+	while (change_due > 0.049999999) {
 		change_due -= 0.5;
 		coins_5 += 1;
 	}
 
-	while (change_due > 0.0099) {
+	while (change_due > 0.009999999) {
 		change_due -= 0.01;
 		coins_1 += 1;
 	}
@@ -110,7 +116,8 @@ int main()
 	cout << bills_10 << " $10 bills" << endl;
 	cout << bills_5 << " $5 bills" << endl;
 	cout << bills_1 << " $1 bills" << endl;
-	cout << bills_100 << " $0.25 coins" << endl;
+    cout << coins_50 << " $0.50 coins" << endl;
+	cout << coins_25 << " $0.25 coins" << endl;
 	cout << coins_10 << " $0.10 coins" << endl;
 	cout << coins_5 << " $0.05 coins" << endl;
 	cout << coins_1 << " $0.01 coins" << endl;
