fce28f4761adb8c391beb5249e9fbe6d433b7429
random-logic <random-logic@users.noreply.github.com>
2019-10-20 15:41:15 -0700

    Finish Lab 5

--- Lab5.cpp
+++ Lab5.cpp
@@ -23,10 +23,10 @@ struct Expense {
 
 void printId(string assignment);
 int getMenuOption();
-void cinNewExpense(Expense expenses[], int size, int max_size);
+int cinNewExpense(Expense expenses[], int size, int max_size);
 void coutAllExpenses(Expense expenses[], int size);
-void coutOneExpense(Expense expenses[], int size);
-void coutAmountExpenses (Expense expenses[], int size);
+void coutExpensesByString(Expense expenses[], int size);
+void coutExpensesByAmount (Expense expenses[], int size);
 
 int main () {
     printId("Lab 5");
@@ -47,39 +47,25 @@ int main () {
 				//coutAllExpenses(expenses, index);
 				//Print out all of the contents of the expenses array
 				for (int i = 0; i < index; i++) {
-					cout.width(20);
-					cout << "AMOUNT(" << expenses[i].amount << ")";
+					cout << "AMOUNT(" << expenses[i].amount << ")   ";
 					cout << "DESC(" << expenses[i].name << ")" << endl;
 				}
 				break;
 			case 2:
-				//cinNewExpense(expenses, index, SIZE);
-					if (index >= SIZE) {
-						cout << "Cannot add more expenses!" << endl;
-					}
-					else {
-						//
-						cout << "Please enter the description for the expense: ";
-						getline(cin, expenses[index].name);
-
-						string buf; //
-
-						cout << "Please enter the amount of the expense: ";
-						getline(cin, buf);
-
-						expenses[index].amount = atof(buf.c_str());
-					}
+				index = cinNewExpense(expenses, index, SIZE);
 				break;
 			case 3:
 				//
-				coutOneExpense(expenses, SIZE);				
+				coutExpensesByString(expenses, index);
 				break;
 			case 4:
-				coutAmountExpenses(expenses, SIZE);
+				coutExpensesByAmount(expenses, index);
 				break;
 			default:
 				return 0;
 		}
+
+		cout << endl; //End line
 	}
 }
 
@@ -101,15 +87,16 @@ void printId(string assignment) {
 }
 
 /**********************************************************************
-* Purpose: 
+* Purpose: Output menu and get user selection for what to do
 *
 * Parameters: None
 *
-* Return: The menu option (int).
+* Return: The menu option (int)
 **********************************************************************/
 int getMenuOption() {
-	string buf;
+	string buf; //String buffer
 
+	//Output and get input
 	cout << "Expense Tracking Menu" << endl;
 	cout << "1) show all" << endl;
 	cout << "2) spend" << endl;
@@ -118,120 +105,124 @@ int getMenuOption() {
 	cout << "5) exit" << endl;
 	getline(cin, buf);
 
-	return atoi(buf.c_str());
+	return atoi(buf.c_str()); //Convert string to int and return
 }
 
 /**********************************************************************
 * Purpose:
 *
-* Parameters: Expense expenses[] -
-*             int size - 
+* Parameters: Expense expenses[] - The expenses array to add an expense to
+*             int index - The index to add the next expense to
+*             int max_size - The max size of the array
 *
-* Return: Nothing
+* Return: The new size of the array (the next available index after input)
 **********************************************************************/
-void cinNewExpense(Expense expenses[], int size, int max_size) {
-	//
-	if (size == max_size) {
+int cinNewExpense(Expense expenses[], int index, int max_size) {
+	//Do not add more expenses if the array is full
+	if (index == max_size) {
 		cout << "Cannot add more expenses!" << endl;
-		return;
+		return index;
 	}
 
-	//
-	cout << "Please enter the description for the expense: " << endl;
-	getline(cin, expenses[size].name);
+	//Get user input
+	cout << "Please enter the description for the expense: ";
+	getline(cin, expenses[index].name);
 
-	float amount; //
-	string buf; //
+	string buf; //String buffer
 
-	cout << "Please enter the amount of the expense: " << endl;
+	//Get more user input
+	cout << "Please enter the amount of the expense: ";
 	getline(cin, buf);
 
-	amount = atof(buf.c_str());
+	//Convert string to float
+	expenses[index].amount = atof(buf.c_str());
+
+	return ++index;
 }
 
 /**********************************************************************
-* Purpose:
+* Purpose: Output all of the expenses
 *
-* Parameters: Expense expenses[] -
-*             int size - 
+* Parameters: Expense expenses[] - The array where all of the expenses is stored
+*             int size - The size of the array
 *
 * Return: Nothing
 **********************************************************************/
 void coutAllExpenses (Expense expenses[], int size) {
 	//Print out all of the contents of the expenses array
 	for (int i = 0; i < size; i++) {
-		cout.setf(ios::left);
-		cout << "AMOUNT(" << expenses[i].amount << ")";
+		cout << "AMOUNT(" << expenses[i].amount << ")   ";
 		cout << "DESC(" << expenses[i].name << ")" << endl;
-		cout.unsetf(ios::left);
 	}
 }
 
 /**********************************************************************
-* Purpose:
+* Purpose: Output the expenses if they contain a part of a certain string
 *
-* Parameters: Expense expenses[] -
-*             int size - 
+* Parameters: Expense expenses[] - The array where all of the expenses is stored
+*             int size - The size of the array
 *
 * Return: Nothing
 **********************************************************************/
-void coutOneExpense(Expense expenses[], int size) {
-	string target; //
+void coutExpensesByString(Expense expenses[], int size) {
+	string target; //The target string to find in the expenses
 
-	//
+	//Get user input
 	cout << "Please enter the search string: ";
 	getline(cin, target);
 
-	//
+	//For all of the expenses, see if there is a match
 	for (int i = 0; i < size; i++) {
-		if (expenses[i].name == target) {
-			cout.setf(ios::left);
-			cout.width(20);
-			cout << "AMOUNT(" << expenses[i].amount << ")";
-			cout << "DESC(" << expenses[i].name << ")" << endl;
-			return;
+		int target_pos = 0; //The position of the letter in the target string to be compared to
+
+		//For each character, see if there is any match within the string
+		for (int j = 0; j < expenses[i].name.size(); j++) {
+			//Do if the target position char != expense position char
+			if (expenses[i].name.at(j) != target.at(target_pos)) {
+				target_pos = 0;
+			}
+			//Do if target position has already covered the whole target string
+			else if (target_pos == target.size() - 1){
+				target_pos = 0;
+
+				cout << "AMOUNT(" << expenses[i].amount << ")   ";
+				cout << "DESC(" << expenses[i].name << ")" << endl;
+
+				break;
+			}
+			//Do if target position has not covered the whole target string yet
+			else {
+				target_pos++;
+			}
 		}
 	}
-
-	cout << "Not Found" << endl; //
 }
 
 /**********************************************************************
-* Purpose:
+* Purpose: Output the expenses if they are >= a specified amount
 *
-* Parameters: Expense expenses[] -
-*             int size - 
+* Parameters: Expense expenses[] - The array where all of the expenses is stored
+*             int size - The size of the array
 *
 * Return: Nothing
 **********************************************************************/
-void coutAmountExpenses (Expense expenses[], int size) {
-	float target_amount = -1; //
+void coutExpensesByAmount (Expense expenses[], int size) {
+	float target_amount; //Target amount to find in the Expenses array
 
-	//
-	while (target_amount < 0) {
-		string buf;
-		cout << "Please enter the amount: ";
-		getline(cin, buf);
-		target_amount = atof(buf.c_str());
-	}
+	string buf; //String buffer
 
-	bool found = false; //
+	//Get user input
+	cout << "Please enter the amount (): ";
+	getline(cin, buf);
 
-	//
+	//Convert string to float
+	target_amount = atof(buf.c_str());
+
+	//For every expense, see if it is greater than the specified amount
 	for (int i = 0; i < size; i++) {
 		if (expenses[i].amount >= target_amount) {
-			found = true;
-
-			cout.width(20);
-			cout << "AMOUNT(" << expenses[i].amount << ")";
-			cout << "DESC(" << expenses[i].name << ")";
-
-			break;
+			cout << "AMOUNT(" << expenses[i].amount << ")   ";
+			cout << "DESC(" << expenses[i].name << ")" << endl;
 		}
-	}
-
-	//
-	if (!found) {
-		cout << "Not Found" << endl;
 	}
 }
