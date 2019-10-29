259ca70e4d0d25fb284f8d827338c1e2db5052b2
Andrew Jowe <andrewjowe@yahoo.com>
2019-10-28 21:04:21 -0700

    Lab6 (NOT COMPLETE)

--- Lab6.cpp
+++ Lab6.cpp
@@ -12,9 +12,6 @@ using std::endl;
 #include <string>
 using std::string;
 
-void coutTableHeader();
-void printId(string assignment);
-
 struct Movie {
 	string title;
 	int viewed;
@@ -22,17 +19,19 @@ struct Movie {
 	Movie* next;
 };
 
+void swapMovies(Movie * p, Movie * q);
+void coutAllMovies(Movie * p);
+void printId(string assignment);
+
 int main() {
-    printId("Lab 6");
+	printId("Lab 6");
 
 	string input = "";
 
 	Movie * first_movie = nullptr;
 	int num_of_movies = 0;
 
-	const int NUM_WIDTH = 2;
-	const int TITLE_WIDTH = 27;
-	const int VIEWED_AND_RATING_WIDTH = 6;
+	cout << endl; //Next line
 
 	while (true) {
 		cout << "Movie menu:" << endl;
@@ -74,26 +73,25 @@ int main() {
 				getline(cin, input);
 				int index = atoi(input.c_str()) - 1;
 
-				Movie * p = first_movie; //The movie to update
-
 				//
 				if (index >= 0 && index < num_of_movies) {
+					Movie * p = first_movie; //The movie to update
+
 					//
-					for (int i = 0; i < index; i++) {
+					for (int i = 0; i < index; i++)
 						p = p->next;
-					}
 
 					//Update the movie at p
-					cout << "The current title for movie " << index + 1 << " is " << p->title;
+					cout << "The current title for movie " << index + 1 << " is " << p->title << endl;
 					cout << "What is the new title for this movie? ";
 					getline(cin, p->title);
 
-					cout << "The current year viewed for movie " << index + 1 << " is " << p->viewed;
+					cout << "The current year viewed for movie " << index + 1 << " is " << p->viewed << endl;
 					cout << "What is the new year viewed for this movie? ";
 					getline(cin, input);
 					p->viewed = atoi(input.c_str());
 
-					cout << "The current rating for movie " << index + 1 << " is " << p->rating;
+					cout << "The current rating for movie " << index + 1 << " is " << p->rating << endl;
 					cout << "What is the new rating for this movie? ";
 					getline(cin, input);
 					p->rating = atoi(input.c_str());
@@ -104,12 +102,17 @@ int main() {
 				break;
 			}
 			case 'E': case 'e': {
+				if (num_of_movies < 1) {
+					cout << "No movies to delete" << endl;
+					break;
+				}
+
 				//Get index to remove
 				cout << "which movie to remove (1-" << num_of_movies << ")? ";
 				getline(cin, input);
 				int index = atoi(input.c_str()) - 1;
 
-				Movie * prev_p; //The previous movie of the movie to remove
+				Movie * prev_p = nullptr; //The previous movie of the movie to remove
 				Movie * p = first_movie; //The movie to remove
 
 				//
@@ -119,12 +122,18 @@ int main() {
 						prev_p = p;
 						p = p->next;
 					}
-
+					
+					//Link next movie of p to first movie
+					if (prev_p == nullptr)
+						first_movie = p->next;
+					//Link next movie of p to next movie of previous movie
+					else
+						prev_p->next = p->next;
+					
 					//Remove the movie at p
-					prev_p->next = p->next;
 					delete p;
 					num_of_movies--;
-					cout << "Removed movie at index " << index << endl;
+					cout << "Removed movie at index " << index + 1<< endl;
 				}
 				else
 					cout << "Invalid index" << endl;
@@ -132,87 +141,88 @@ int main() {
 				break;
 			}
 			case 'L': case 'l': {
-				coutTableHeader();
-
-				int movie_count = 0; //
-				Movie * p = first_movie; //Pointer to the movie to print out
-				
-				//
-				for (int i = 0; i < num_of_movies; i++) {
-					cout.width(NUM_WIDTH);
-					cout << ++movie_count;
-					cout << " ";
-
-					cout.width(TITLE_WIDTH);
-					cout << p->title;
-					cout << " ";
-
-					cout.width(VIEWED_AND_RATING_WIDTH);
-					cout << p->viewed;
-					cout << " ";
-
-					cout.width(VIEWED_AND_RATING_WIDTH);
-					cout << p->rating;
-					cout << " ";
-
-					cout << endl;
-
-					p = p->next;
-				}
+				coutAllMovies(first_movie);
 				break;
 			}
 			case 'T': case 't': {
+				//CASE INSENSITIVE
+				//Get how the user wants to sort the movies
+				cout << "Arrange by lowest value (A) to highest value (Z) string (Enter \'L\') or oldest to newest (Enter \'O\')? ";
+				getline(cin, input);
+
+				//
+				if (input.at(0) == 'O' || input.at(0) == 'o') {
+					for (Movie * p = first_movie; p != nullptr; p = p->next)
+						for (Movie * q = p->next; q != nullptr; q = q->next) //CONTINUE
+					coutAllMovies(first_movie);
+				}
+				else if (input.at(0) == 'N' || input.at(0) == 'n') {
+					for (Movie * p = first_movie; p != nullptr; p = p->next)
+						for (Movie * q = p->next; q != nullptr; q = q->next) //CONTINUE
+					coutAllMovies(first_movie);
+				}
+				else
+					cout << "Invalid input" << endl;
 				break;
 			}
 			case 'V': case 'v': {
 				//Get how the user wants to sort the movies
-				cout << "Arrange by newest to oldest (Enter N) or oldest to newest (Enter O)? ";
+				cout << "Arrange by newest to oldest (Enter \'N\') or oldest to newest (Enter \'O\')? ";
 				getline(cin, input);
 
-				bool swapped = true; //Find out whether the lists has swapped or not
-
 				//
-				/*if (input.at(0) == 'O' || input.at(0) == 'o') {
-					//
-					while (swapped) {
-						swapped = false;
-
-						Movie * p = first_movie;
-						Movie * prev_p;
-
-						//
-						for (int i = 0; i < num_of_movies; i++) {
-							if (p->viewed > p->next->viewed) {
-								swapped = true;
-
-								Movie * temp = p;
-								p->next = p;
-							}
-
-							p = p->next;
-							prev_p = p;
-						}
-					}
+				if (input.at(0) == 'O' || input.at(0) == 'o') {
+					for (Movie * p = first_movie; p != nullptr; p = p->next)
+						for (Movie * q = p->next; q != nullptr; q = q->next)
+							if (q->viewed > p->viewed)
+								swapMovies(p, q);
+					coutAllMovies(first_movie);
 				}
 				else if (input.at(0) == 'N' || input.at(0) == 'n') {
-
+					for (Movie * p = first_movie; p != nullptr; p = p->next)
+						for (Movie * q = p->next; q != nullptr; q = q->next)
+							if (q->viewed < p->viewed)
+								swapMovies(p, q);
+					coutAllMovies(first_movie);
 				}
 				else
-					cout << "Invalid input" << endl;*/
+					cout << "Invalid input" << endl;
 
 				break;
 			}
 			case 'R': case 'r': {
+				//Get how the user wants to sort the movies
+				cout << "Arrange by highest to lowest (Enter \'H\') or lowest to highest (Enter \'L\')? ";
+				getline(cin, input);
+
+				//
+				if (input.at(0) == 'H' || input.at(0) == 'h') {
+					for (Movie * p = first_movie; p != nullptr; p = p->next)
+						for (Movie * q = p->next; q != nullptr; q = q->next)
+							if (q->rating > p->rating)
+								swapMovies(p, q);
+					coutAllMovies(first_movie);
+				}
+				else if (input.at(0) == 'L' || input.at(0) == 'l') {
+					for (Movie * p = first_movie; p != nullptr; p = p->next)
+						for (Movie * q = p->next; q != nullptr; q = q->next)
+							if (q->rating < p->rating)
+								swapMovies(p, q);
+					coutAllMovies(first_movie);
+				}
+				else
+					cout << "Invalid input" << endl;
+
 				break;
 			}
 			case 'Q': case 'q': {
 				//Delete the whole movie list
-				for (int i = 0; i < num_of_movies; i++) {
+				while (first_movie != nullptr) {
 					Movie * p = first_movie;
 					first_movie = first_movie->next;
 					delete p;
 				}
-				
+				cout << endl << "Program Finished!" << endl;
 				return 0;
 			}
 			default:
@@ -224,15 +234,41 @@ int main() {
 }
 
 /**********************************************************************
-* Purpose: Print out the header of the movie table
+* Purpose: Print out all of the movies in a nicely formatted table
 *
-* Parameters: None
+* Parameters: Movie * p - The pointer to the first movie
 *
 * Return: Nothing
 **********************************************************************/
-void coutTableHeader() {
+void coutAllMovies(Movie * p) {
+	const int NUM_WIDTH = 2; //The width of the number column
+	const int TITLE_WIDTH = 27; //The width of the title column
+	const int VIEWED_AND_RATING_WIDTH = 6; //The width of the viewed and rating column
+
 	cout << "#  Title                         Viewed Rating" << endl;
 	cout << "-- ---------------------------   ------ ------" << endl;
+
+	int movie_count = 0; //The count of the movie
+
+	for (p; p != nullptr; p = p->next) {
+		cout.width(NUM_WIDTH);
+		cout << ++movie_count;
+		cout << " ";
+
+		cout.width(TITLE_WIDTH);
+		cout << p->title;
+		cout << " ";
+
+		cout.width(VIEWED_AND_RATING_WIDTH);
+		cout << p->viewed;
+		cout << " ";
+
+		cout.width(VIEWED_AND_RATING_WIDTH);
+		cout << p->rating;
+		cout << " ";
+
+		cout << endl;
+	}
 }
 
 /**********************************************************************
@@ -250,4 +286,22 @@ void printId(string assignment) {
 	cout << "Compiler Used: Visual Studio Developer Command Prompt" << endl;
 	cout << "File: " << __FILE__ << endl;
 	cout << "Compiled: " << __DATE__ << endl;
+}
+
+/**********************************************************************
+* Purpose: Swap the movies within the linked list
+*
+* Parameters: Movie * p - The first movie to swap
+*             Movie * q - The second movie to swap
+*
+* Return: Nothing
+**********************************************************************/
+void swapMovies (Movie * p, Movie * q) {
+	Movie * temp;
+	temp = p;
+	p = q;
+	q = temp;
+	temp = p->next;
+	p->next = q->next;
+	q->next = temp;
 }
